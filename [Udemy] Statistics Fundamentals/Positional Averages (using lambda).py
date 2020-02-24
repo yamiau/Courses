6c67a981657ba0c968136e1d5b8831d5e13a7de4
Yami <mu.meiraaz@gmail.com>
2020-02-24 17:25:52 -0300

    Added Frequency to Median, having issues on the lambda versions

--- [Udemy] Statistics Fundamentals/Positional Averages (using lambda).py
+++ [Udemy] Statistics Fundamentals/Positional Averages (using lambda).py
@@ -10,6 +10,13 @@ def median(values):
 	else:
 		index = (int(len(values)/2))
 		return(values[index])
+
+def median2(values, frequencies):
+	merged_set = [(values[i], frequencies[i]) for i in range(len(values))]
+	print(merged_set)
+	sort = lambda v: v and [i for i in v[1:] if i[0] < v[0][0]] + [v[0][0]] + [i for i in v[1:] if i[0] > v[0][0]]
+	merged_set = sort(merged_set)
+	print(merged_set)
 	
 def mode(oneD):
 	copy = oneD
@@ -19,4 +26,5 @@ def mode(oneD):
 	return(None)
 	
 mine = [100, 20, 11, 53, 67, 80]
-print(median(mine))
+freqs = [3, 53, 68, 12, 2, 45]
+median2(mine, freqs)
--- [Udemy] Statistics Fundamentals/Positional Averages.py
+++ [Udemy] Statistics Fundamentals/Positional Averages.py
@@ -9,6 +9,20 @@ def median(values):
 	else:
 		index = (int(len(values)/2))
 		return(values[index])
+
+def median2(values, frequencies):
+	merged_set = [(values[i], frequencies[i]) for i in range(0, len(values))]
+	merged_set = quick_sort2(merged_set)
+	cumulative_freq =[]
+	for i in range(len(merged_set)):
+		if i > 0:
+			cumulative_freq.append(merged_set[i][1] + cumulative_freq[i-1])
+		else:
+			cumulative_freq.append(merged_set[i][1])
+	c = cumulative_freq[-1]/2
+	for i in range(len(cumulative_freq)):
+		if cumulative_freq[i] >= c:
+			return merged_set[i][0]
 	
 def mode(oneD):
 	copy = oneD
@@ -27,6 +41,18 @@ def quick_sort(values):
 		return quick_sort(smaller) + [pivot] + quick_sort(greater)
 	else:
 		return(values)
+
+def quick_sort2(values):
+	if values:
+		pivot = values[0][0]
+		smaller = [i for i in values[1:] if i[0] < pivot]
+		greater = [i for i in values[1:] if i[0] >= pivot]
+		return quick_sort2(smaller) + [values[0]] + quick_sort(greater)
+	else:
+		return(values)
+#
 	
-mine = [10, 20, 11, 53, 67, 80]
-print(quick_sort(mine))
+values = [1, 6, 88, 10, 35]
+frequencies = [50, 26, 2, 31, 5]
+
+print(median2(values, frequencies))
